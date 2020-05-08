2836c8767e848bbe84b74c865e5e97c43bed03b3
Julius <Julius>
2020-05-08 14:18:58 +0300

    First initial commit

---
+++ src/Game_of_Life.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Julius
+ */
+public class Game_of_Life {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
