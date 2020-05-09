8968b8d867d7f80de261575177cbda306b9de4ec
Julius <Julius>
2020-05-09 11:41:26 +0300

    Create first iteration and Print / Start

--- src/Game_of_Life.java
+++ src/Game_of_Life.java
@@ -9,24 +9,29 @@ public class Game_of_Life {
 
     public static void main(String[] args) {
         
-        int dydis = 5;
-        char[][] laukas = new char[dydis][dydis];
-        
-        for(int x=0; x<laukas.length; x++) {
-            for(int y=0; y<laukas.length; y++) {
-                if (Math.random() < 0.22) {
-                    laukas[x][y] = 'X';
-                } else {
-                    laukas[x][y] = '.';
-                }
-            }
-        }
-        for(int x=0; x<laukas.length; x++) {
-            for(int y=0; y<laukas.length; y++) {
-                System.out.print(laukas[x][y]);
-            }
-            System.out.println();
-        }       
+         int cords = 5;
+         char[][] laukas = new char[cords][cords];
+         // Sugeneruojamas pirmas laukas
+         for(int x=0; x<laukas.length; x++) {
+             for(int y=0; y<laukas.length; y++){
+                 if(Math.random() < 0.22) {
+                     laukas[x][y] = 'X';
+                 } else {
+                     laukas[x][y] = '.';
+                 }
+             }
+         }
+         // Spausdina pirma sugeneruota lauka
+         for(int x=0; x<laukas.length; x++) {
+             for(int y=0; y<laukas.length; y++) {
+                 System.out.print(laukas[x][y]);
+             }
+             System.out.println();
+         }
+         
+         for(int kiek=0; kiek<50; kiek++) {
+             
+         }
         
     }
     
